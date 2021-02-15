dad971a28bf67751192108ad2415df6f5a0463a6
Nicollas <contatonicollasfeitosa@gmail.com>
2021-02-15 14:24:55 -0300

    update tailwind and laravel mix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-    purge: [
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+    purge: {
+        content: [
+            './vendor/laravel/jetstream/**/*.blade.php',
+            './storage/framework/views/*.php',
+            './resources/views/**/*.blade.php',
+        ]
+    },
 
     theme: {
         extend: {
+            colors: {
+                primary: colors.indigo,
+                'cool-gray': colors.coolGray,
+            },
+
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
@@ -16,6 +24,11 @@ module.exports = {
     },
 
     variants: {
+        textColor: ['group-hover'],
+        backgroundColor: ['even', 'odd'],
+        scale: ['active'],
+        boxShadow: ['active'],
+
         extend: {
             opacity: ['disabled'],
         },
